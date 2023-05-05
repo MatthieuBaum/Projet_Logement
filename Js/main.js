d791c055230d39607393f45522c91453979f73ac
MatthieuBaum <90194395+MatthieuBaum@users.noreply.github.com>
2023-05-05 14:41:49 +0200

    bjbj

--- Js/main.js
+++ Js/main.js
@@ -1,79 +1,54 @@
-var i = 0;
-var a = 0;
-var isBackspacing = false;
-var isParagraph = false;
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-var textArray = [
-    "une maison","un appartement"
-];
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-
-var speedForward = 100; 
-var  speedWait = 1000;
-var  speedBetweenLines = 1000;
-var  speedBackspace = 25; 
-
-
-typeWriter("output", textArray);
-function typeWriter(id, ar) {
-  var element = $("#" + id);
-  var  aString = ar[a];
-  var  eHeader = document.querySelector('.cursor');
-  var  eParagraph = document.querySelector('.mot'); 
-
-  if (!isBackspacing) {
-    
-    if (i < aString.length) {
-      
-      if (aString.charAt(i) == "|") {
-        isParagraph = true;
-        eHeader.removeClass("cursor");
-        eParagraph.addClass("cursor");
-        i++;
-        setTimeout(function(){ typeWriter(id, ar); }, speedBetweenLines);
-        
-      } else {
-        if (!isParagraph) {
-          eHeader.text(eHeader.text() + aString.charAt(i));
-        } else {
-          eParagraph.text(eParagraph.text() + aString.charAt(i));
-        }
-        i++;
-        setTimeout(function(){ typeWriter(id, ar); }, speedForward);
-      }
-      
-    } else if (i == aString.length) {
-      
-      isBackspacing = true;
-      setTimeout(function(){ typeWriter(id, ar); }, speedWait);
-      
-    }
-  
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
-    
-    if (eHeader.text().length > 0 || eParagraph.text().length > 0) {
-      
-      if (eParagraph.text().length > 0) {
-        eParagraph.text(eParagraph.text().substring(0, eParagraph.text().length - 1));
-      } else if (eHeader.text().length > 0) {
-        eParagraph.removeClass("cursor");
-        eHeader.addClass("cursor");
-        eHeader.text(eHeader.text().substring(0, eHeader.text().length - 1));
-      }
-      setTimeout(function(){ typeWriter(id, ar); }, speedBackspace);
-    
-    
-    } else { 
-      
-      isBackspacing = false;
-      i = 0;
-      isParagraph = false;
-      a = (a + 1) % ar.length; 
-      setTimeout(function(){ typeWriter(id, ar); }, 50);
-      
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-}
+};
 
 const slider = document.querySelector('.gallery');
 let isDown = false;
